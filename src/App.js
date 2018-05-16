bbf52d50cb0d06a2a6f2f007ff34584bacb63573
OleksandrMitrakov <mitrakovoleksandr@gmail.com>
2018-05-16 13:06:52 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sass/main.css';
+
+
+class App extends React.Component {
+
+
+
+    render() {
+        return <h1>Hello :)</h1>
+    }
+}
+
+export default App;
+
+
